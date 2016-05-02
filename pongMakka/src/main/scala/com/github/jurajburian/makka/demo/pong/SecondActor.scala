90e9f1874f8e3ad8035af60fe01a5c5c67fd1931
Vaclav Svejcar <vaclav.svejcar@finfactory.com>
2016-05-02 15:46:50 +0200

    Minor improvements

--- pongMakka/src/main/scala/com/github/jurajburian/makka/demo/pong/SecondActor.scala
+++ pongMakka/src/main/scala/com/github/jurajburian/makka/demo/pong/SecondActor.scala
@@ -3,10 +3,12 @@ package com.github.jurajburian.makka.demo.pong
 import akka.actor.{Actor, ActorRef}
 import com.github.jurajburian.makka.demo.shared.PingPongMessage
 
+import scalaz.Scalaz._
+
 class SecondActor(thirdActor: ActorRef, sysName: String) extends Actor {
 
 	override def receive: Receive = {
-		case m@PingPongMessage(passed) => thirdActor ! m.copy(sign(passed))
+		case m@PingPongMessage(passed) => thirdActor ! m.copy(passed |> sign)
 	}
 
 	private def sign(passed: List[String]): List[String] =
